5c24d9be86edac664caca6fdc02b612404c3b039
rorist <rorist@0xcafe.ch>
2017-12-12 21:18:07 +0100

    complete rewrite using ws2812fx

--- ha_mqtt_ledstrip.ino
+++ ha_mqtt_ledstrip.ino
@@ -8,11 +8,12 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-#include <Adafruit_NeoPixel.h>
 #define PIN 14
 #define NUM_LEDS 300
 #define BRIGHTNESS 50
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+
+#include <WS2812FX.h>
+WS2812FX ws2812fx = WS2812FX(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
 
 const bool debug_mode = CONFIG_DEBUG;
 
@@ -43,30 +44,14 @@ bool stateOn = false;
 const char* setEffect = NULL;
 String currentEffect = "";
 
-// Effect data
-byte neopix_gamma[] = {
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
-
 void setup() {
   
-  strip.setBrightness(BRIGHTNESS);
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  ws2812fx.init();
+  ws2812fx.setBrightness(BRIGHTNESS);
+  ws2812fx.setSpeed(200);
+  ws2812fx.setMode(FX_MODE_STATIC);
+  ws2812fx.setColor(0, 0, 0);
+  ws2812fx.start();
   
   if (debug_mode) {
     Serial.begin(115200);
@@ -76,6 +61,8 @@ void setup() {
   setup_ota();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+  sendState();
 
 }
 
@@ -187,20 +174,29 @@ bool processJson(char* message) {
     }
     else if (strcmp(root["state"], off_cmd) == 0) {
       stateOn = false;
+      ws2812fx.setColor(0, 0, 0);
     }
   }
 
   if (root.containsKey("effect")) {
     setEffect = root["effect"];
     currentEffect = setEffect;
+    if(currentEffect == "wipe") {
+      ws2812fx.setMode(FX_MODE_BREATH);
+    }
+    else if(currentEffect == "solid") {
+      ws2812fx.setMode(FX_MODE_STATIC);
+    }
   }
 
   if (root.containsKey("color")) {
     red = root["color"]["r"];
     green = root["color"]["g"];
     blue = root["color"]["b"];
+    ws2812fx.setColor(red, green, blue);
   }
 
+  /*
   if (root.containsKey("white_value")) {
     white = root["white_value"];
   }
@@ -208,6 +204,7 @@ bool processJson(char* message) {
   if (root.containsKey("brightness")) {
     brightness = root["brightness"];
   }
+  */
   
 
   return true;
@@ -261,111 +258,12 @@ void reconnect() {
   }
 }
 
-
-/*
- * LOOP
- */
-
-
 void loop() {
-  
   ArduinoOTA.handle();
-
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
-  if(stateOn) {
-    if(currentEffect != "") {
-      if(currentEffect == "wipe") {
-        colorWipe();
-      }
-      else if(currentEffect == "rainbow") {
-        rainbow();
-      }
-      else if(currentEffect == "pulse") {
-        pulse();
-      }
-      else if(currentEffect == "solid") {
-        setColor(red, green, blue, white);
-      }
-    } else {
-      setColor(red, green, blue, white);
-    }
-  } else {
-    setColor(0, 0, 0, 0);
-  }
-}
-
-
-/*
- *  Effects
- */
-
-void setColor(int inR, int inG, int inB, int inW) {
-  for(int i=0; i< strip.numPixels(); i++) {
-    strip.setPixelColor(i, inR, inG, inB, inW );
-  }
-  strip.show();
-}
-
-// Fill the dots one after the other with a color
-void colorWipeSingle(uint32_t c) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(20);
-  }
-}
-void colorWipe() {
-  colorWipeSingle(strip.Color(255, 0, 0));    // Red
-  //colorWipeSingle(strip.Color(0, 255, 0));    // Green
-  colorWipeSingle(strip.Color(0, 0, 255));    // Blue
-  //colorWipeSingle(strip.Color(0, 0, 0, 255)); // White
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
-}
-
-void rainbow() {
-  uint16_t i, j;
-  for(j=0; j<256 * 5; j++) {
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(50);
-  }
-}
-
-void pulse() {
-  for(int j = 0; j < 256 ; j++){
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-          strip.setPixelColor(i, strip.Color(0,0,0, neopix_gamma[j] ) );
-        }
-        delay(5);
-        strip.show();
-      }
-
-  for(int j = 255; j >= 0 ; j--){
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-          strip.setPixelColor(i, strip.Color(0,0,0, neopix_gamma[j] ) );
-        }
-        delay(5);
-        strip.show();
-      }
+  ws2812fx.service();
 }
 
